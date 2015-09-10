5f0c0be2227ed50792601f0da1c18e4165ca4cee
Jie Yang <contact@yangjie.me>
2015-09-10 22:58:12 +0930

    add zsh plugin

--- ubuntu.sh
+++ ubuntu.sh
@@ -10,6 +10,7 @@ sudo passwd ubuntu
 git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
 sed -i '/^ZSH_THEME="robbyrussell"/s/robbyrussell/clean/' ~/.zshrc
+sed -i '/^plugins=/s/)/ ubuntu)/' ~/.zshrc
 chsh -s $(which zsh)
 
 wget https://3230d63b5fc54e62148e-c95ac804525aac4b6dba79b00b39d1d3.ssl.cf1.rackcdn.com/Anaconda-2.3.0-Linux-x86_64.sh
@@ -19,3 +20,6 @@ echo 'export PATH="/home/ubuntu/anaconda/bin:$PATH"' >> ~/.zshrc
 # mkdir mount_point
 # sudo mount /dev/xvdf mount_point
 # chown ubuntu:ubuntu mount_point
+
+env zsh
+. ~/.zshrc
