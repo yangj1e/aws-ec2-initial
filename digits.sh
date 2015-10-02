4004d558012236578b9fa4e8491aeca6752316b2
Jie Yang <contact@yangjie.me>
2015-10-02 18:04:02 +0930

    use pip if no anaconda

--- digits.sh
+++ digits.sh
@@ -3,7 +3,12 @@
 cd ~/
 git clone https://github.com/NVIDIA/DIGITS.git
 
-conda install -y protobuf flask-wtf gunicorn pydot
-pip install lmdb flask-socketio
+if hash conda 2>/dev/null ; then
+    conda install -y protobuf flask-wtf gunicorn pydot
+    pip install lmdb flask-socketio
+else
+    cd DIGITS/
+    pip install -r requirements.txt
+fi
 
 sudo apt-get install -y graphviz
