5d006dd8e14c760c06769bea384e0bba5a52d741
Jie Yang <contact@yangjie.me>
2015-11-08 01:34:06 +1030

    add notebook.sh

---
+++ notebook.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+jupyter notebook --generate-config
+
+key=$(python -c "from notebook.auth import passwd; print(passwd())")
+
+cd ~/.jupyter
+sed -i "1 a\
+c = get_config()\\
+c.NotebookApp.ip = '*'\\
+c.NotebookApp.open_browser = False\\
+c.NotebookApp.password = u'$key'\\
+c.NotebookApp.port = 8888" jupyter_notebook_config.py
