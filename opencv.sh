9a61445f7391ab4ca49629adc47d59a2fe2745d6
Jie Yang <contact@yangjie.me>
2015-11-14 15:35:31 +1030

    Fix OpenCV installation
    Remove libdc1394, add libatlas-base-dev

--- opencv.sh
+++ opencv.sh
@@ -1,16 +1,19 @@
-sudo apt-get install -y software-properties-common
-sudo add-apt-repository -y ppa:george-edison55/cmake-3.x
 sudo apt-get update
+sudo apt-get install -y build-essential \
+    cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev \
+    libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libatlas-base-dev
+sudo apt-get clean
 
-sudo apt-get install -y build-essential cmake git pkg-config \
-    libjpeg8-dev libtiff4-dev libjasper-dev libpng12-dev libgtk2.0-dev \
-    libavcodec-dev libavformat-dev libswscale-dev libv4l-dev \
-    libatlas-base-dev gfortran
-
+sudo bash -c 'cat <<EOF > /etc/init.d/create_build_dir
+#!/bin/bash
 if [ -d /mnt ] && [ ! -e /mnt/build_tmp ] ; then
   mkdir /mnt/build_tmp
   chown ubuntu /mnt/build_tmp
 fi
+EOF'
+sudo chmod 744 /etc/init.d/create_build_dir
+sudo /etc/init.d/create_build_dir
+sudo update-rc.d create_build_dir defaults
 
 cd /mnt/build_tmp
 git clone https://github.com/Itseez/opencv.git
@@ -33,7 +36,6 @@ cmake -D CMAKE_BUILD_TYPE=RELEASE \
       -D PYTHON_LIBRARY=/home/ubuntu/anaconda2/lib/libpython2.7.so \
       -D PYTHON_PACKAGES_PATH=/home/ubuntu/anaconda2/lib/python2.7/site-packages \
       -D BUILD_EXAMPLES=ON \
-      -D PYTHON2_LIBRARY=/home/ubuntu/anaconda2/lib/libpython2.7.so \
       -D BUILD_opencv_python3=OFF \
       -D BUILD_opencv_python2=ON ..
 
